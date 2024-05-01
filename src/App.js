d1fcb5c0c95003732e8855ea24f2c99fe4db75d4
Sayed Atif <atif.ghafaar@gmail.com>
2024-05-01 13:11:46 -0500

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// import './index.css'
+// import EmployeeList from './components/EmployeeList'
+// import EmployeeListItem from './components/EmployeeListItem'
+import EmployeePage from './components/EmployeePage'
+// import Header from './components/Header'
+import HomePage from './components/HomePage'
+// import SearchBar from './components/SearchBar'
 
+
+import React from 'react'
 function App() {
+  const topics = ["Employee Directory", "Employee"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HomePage title={topics[0]}/>
+    <EmployeePage title={topics[1]}/>
+  </div>
+  )
 }
 
-export default App;
+
+
+export default App
